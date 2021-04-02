96d068368725a617c9b08a5354081e077ace5616
AE <alexander.w.east@gmail.com>
2021-04-02 11:48:48 -0400

    added roxygen documentation

--- R/LEM.R
+++ R/LEM.R
@@ -1,20 +1,19 @@
-# Lorber-Egeghy Model for Exposure Analysis
-#
-# This is an example function named 'LEM'
-# which returns the exposure distribution given a dataframe.
-#
-# You can learn more about this method at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#' Lorber-Egeghy Model for Exposure Analysis
+#'
+#' This is an example function named 'LEM'
+#' which returns the exposure distribution given a dataframe.
+#'
+#' You can learn more about this method at:
+#'
+#'   http://r-pkgs.had.co.nz/
+#'
+#'  Some useful keyboard shortcuts for package authoring:
+#'
+#'   Install Package:           'Ctrl + Shift + B'
+#'   Check Package:             'Ctrl + Shift + E'
+#'   Test Package:              'Ctrl + Shift + T'
 
 LEM <- function() {
   print("Add function here")
 }
-
 
