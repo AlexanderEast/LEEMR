704a56f1b9c9850b285e93d6382179ba3c7e3182
AE <alexander.w.east@gmail.com>
2021-04-05 18:30:31 -0400

    Added Example File (Excel) and null option for weight input

--- R/LEM.R
+++ R/LEM.R
@@ -3,8 +3,23 @@
 # Learn more by entering "?LorberEgeghyModel::LEM"
 # into the console or running the line in R.
 
-LEM <- function() {
-  print("Hello, LEM!")
+## AE: MENTION UNITS
+
+library('rio')
+data <- rio::import("PFOA Drinking Water.xlsx",sheet ="PFOA Drinking Water" )
+waterfactors <- 1.4*0.9 #(L/DAY INTAKE * Absorption Factor)
+
+
+
+LEM <- function(df,wt = NULL,ef,n){
+  if(is.null(wt)){
+    wt <- 1
+  }
+
+  return(wt)
 }
+
+LEM(df = 5, ef = 2, n = 6)
+
 
 
