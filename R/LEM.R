1a7c77a865082b52d01fe63428e64c14668370cc
AE <alexander.w.east@gmail.com>
2021-04-06 13:29:07 -0400

    Transferred LEM from previous efforts to this package. (Version 0.1)

--- R/LEM.R
+++ R/LEM.R
@@ -3,23 +3,292 @@
 # Learn more by entering "?LorberEgeghyModel::LEM"
 # into the console or running the line in R.
 
-## AE: MENTION UNITS
 
+rm(list = ls())
 library('rio')
 data <- rio::import("PFOA Drinking Water.xlsx",sheet ="PFOA Drinking Water" )
-waterfactors <- 1.4*0.9 #(L/DAY INTAKE * Absorption Factor)
+library('dplyr')
+wt <- "Sample Size"
+n  <- 200
+ef <- 1.4*9#(L/DAY INTAKE * Absorption Factor)
+expunits <- "ng/day"
+seed <- 12345
+#1. Units
+data<- data %>%mutate(UNITFACTOR = case_when(
+  (Units %in% c("ng/m³","ng/L","ng/g","µg/kg","ug/kg","pg/mL","pg/ml")) ~ 1,
+  (Units %in% c("pg/m³","pg/g")) ~ 0.001,
+  (Units %in% c("ng/mL","ug/l","µg/L","ug/m³","µg/m³")) ~ 1000)) %>%
+  mutate_at(c("Min","Max","Median","Mean","SD","GM","GSD","P10","P25","P75","P90","P95","P99"),~.*UNITFACTOR) %>%
+  mutate(Units = case_when(
+    (Units %in% c("ug/m3","µg/m³","pg/m³","ng/m³")) ~ "ng/m³",
+    (Units %in% c("ng/mL","ug/l","ug/L","µg/l","µg/L","pg/ml","pg/mL","ng/L")) ~ "ng/L",
+    (Units %in% c("pg/g","µg/kg","ug/kg","ng/g")) ~ "ng/g")) %>%
+  select(-UNITFACTOR)
+
+#2. GM/GSD Estimator
+# A. Estimate GM using Pleil 1.
+data <- data %>% mutate(GM = if_else(!is.na(GM),GM , Median))
+
+# B. Estimate GM using Pleil 2.
+data <- data %>% mutate(GM = if_else(!is.na(GM),GM , Mean/(1+0.5 *(SD/Mean)^2)))
+
+# C. Estimate GSD using Pleil 1.
+data <- data %>% mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P10/GM)/qnorm(.10)))) %>%
+  mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P25/GM)/qnorm(.25)))) %>%
+  mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P75/GM)/qnorm(.75)))) %>%
+  mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P90/GM)/qnorm(.90)))) %>%
+  mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P95/GM)/qnorm(.95)))) %>%
+  mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P99/GM)/qnorm(.99))))
+
+# D. Estimate GSD using Pleil 2.
+data <- suppressWarnings(data %>% mutate(GSD = if_else(!is.na(GSD),GSD ,exp(sqrt(2 * log(Mean/GM))))))
+# E. Estimate GM using Pleil 3.
+data <- data %>% mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(Max/GM)/qnorm(1-1/`Sample Size`))))
+data <- data %>% mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(Min/GM)/qnorm(1/`Sample Size`))))
+
+# F. Estimate Mean and SD using "Estimating.datalsdata" Methods (5) and (16). Mean calculated from min, median, Maximum,
+# and SD from minimum, median, Maximum, and range.
+
+data <- data %>% mutate(Mean = if_else(!is.na(Mean),Mean, (Min+2*Median+Max)/4))
+data <- data %>% mutate(SD = if_else(!is.na(SD),SD, sqrt ((1/12) * ((Min-2*Median+Max)^2)/4 + (Max-Min)^2)))
 
 
+# G. Estimate SD using Ramirez & Codata Method and range rule. Applied only if sample size  > 10.
+data <- data %>% mutate(SD = if_else((!is.na(SD) & `Sample Size` > 10),SD, (Max-Min)/ (3*sqrt(log(`Sample Size`))-1.5)))
+data <- data %>% mutate(SD = if_else((!is.na(SD) & `Sample Size` > 10),SD, (Max-Min)/4))
 
-LEM <- function(df,wt = NULL,ef,n){
-  if(is.null(wt)){
-    wt <- 1
-  }
+# ______________________________ Repeat A - E. ______________________________ #
 
-  return(wt)
+
+# H. Estimate GM using Pleil 1.
+data <- data %>% mutate(GM = if_else(!is.na(GM),GM , Median))
+
+# I. Estimate GM using Pleil 2.
+data <- data %>% mutate(GM = if_else(!is.na(GM),GM , Mean/(1+0.5 *(SD/Mean)^2)))
+
+# J. Estimate GSD using Pleil 1.
+data <- data %>% mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P10/GM)/qnorm(.10)))) %>%
+  mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P25/GM)/qnorm(.25)))) %>%
+  mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P75/GM)/qnorm(.75)))) %>%
+  mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P90/GM)/qnorm(.90)))) %>%
+  mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P95/GM)/qnorm(.95)))) %>%
+  mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P99/GM)/qnorm(.99))))
+
+# K. Estimate GSD using Pleil 2.
+data <- suppressWarnings(data %>% mutate(GSD = if_else(!is.na(GSD),GSD ,exp(sqrt(2 * log(Mean/GM))))))
+
+# L. Estimate GM using Pleil 3.
+data <- data %>% mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(Max/GM)/qnorm(1-1/`Sample Size`))))
+data <- data %>% mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(Min/GM)/qnorm(1/`Sample Size`))))
+
+
+df <- data.frame(A=1:10, B=2:11, C=3:12)
+fun1 <- function(x, column){
+  max(x[,column])
 }
 
-LEM(df = 5, ef = 2, n = 6)
+set.seed(seed)
+wgm  <- weighted.mean(data$GM, data[,wt])
+wgsd <- weighted.mean(data$GSD, data[,wt])
+
+Concentration<- rlnorm(n,wgm,wgsd)
+Exposure     <- rlnorm(n,wgm,wgsd)*ef
+
+sumstats<- function(x){
+sumstats <- t(quantile(x,c(0,.10,.5,.75,.95,1)))
+mymean   <- mean(x)
+sumstats <- cbind(mymean,sumstats)
+colnames(sumstats)<-c("Mean","Min","10th%","Median","75th%","95th","Max")
+sumstats<- signif(sumstats,5)
+return(sumstats)
+}
+
+summary <- rbind(sumstats(Concentration),sumstats(Exposure))
+summary <- cbind(c("Concentration", "Exposure"),summary)
+colnames(summary)[1] <- "Result"
 
 
+Concentration <- data.frame(Concentration)
+Concentration$Units <- names(table(data$Units))
+
+Exposure <- data.frame(Exposure)
+Exposure$Units <- expunits
+
+namer <- function(name){
+
+  date   <- Sys.time()
+  year   <- substr(date,1,4)
+  month  <- substr(date,6,7)
+  day    <- substr(date,9,10)
+  hour   <- as.numeric(substr(date,12,13))
+  minute <- substr(date,15,16)
+
+  if (hour > 12){
+    hour <- as.character(hour-12)
+    minute <- paste0(minute,"PM")
+  } else {
+    minute <- paste0(minute,"AM")
+  }
+
+  filename <- paste0(name," ",hour,minute," ",month," ",day," ",year)
+
+  return(filename)
+}
+time  <- namer('Run Date:')
+description <- t(data.frame(Info =time,
+                n,
+                WGM = wgm,
+                WGSD= wgsd,
+                Weight = wt,
+                Seed = seed))
+
+
+
+# return( sumstats, dist, data, cover)
+results<-list(description,summary,Exposure,Concentration)
+names(results)<- c("Description","Summary Statistics","Exposure","Concentration")
+
+
+rm(list=ls())
+LEM <- function(df,wt,expf,expunits, n, seed){
+
+  data<- df
+  data<- data %>%mutate(UNITFACTOR = case_when(
+    (Units %in% c("ng/m³","ng/L","ng/g","µg/kg","ug/kg","pg/mL","pg/ml")) ~ 1,
+    (Units %in% c("pg/m³","pg/g")) ~ 0.001,
+    (Units %in% c("ng/mL","ug/l","µg/L","ug/m³","µg/m³")) ~ 1000)) %>%
+    mutate_at(c("Min","Max","Median","Mean","SD","GM","GSD","P10","P25","P75","P90","P95","P99"),~.*UNITFACTOR) %>%
+    mutate(Units = case_when(
+      (Units %in% c("ug/m3","µg/m³","pg/m³","ng/m³")) ~ "ng/m³",
+      (Units %in% c("ng/mL","ug/l","ug/L","µg/l","µg/L","pg/ml","pg/mL","ng/L")) ~ "ng/L",
+      (Units %in% c("pg/g","µg/kg","ug/kg","ng/g")) ~ "ng/g")) %>%
+    select(-UNITFACTOR)
+
+  #2. GM/GSD Estimator
+  # A. Estimate GM using Pleil 1.
+  data <- data %>% mutate(GM = if_else(!is.na(GM),GM , Median))
+
+  # B. Estimate GM using Pleil 2.
+  data <- data %>% mutate(GM = if_else(!is.na(GM),GM , Mean/(1+0.5 *(SD/Mean)^2)))
+
+  # C. Estimate GSD using Pleil 1.
+  data <- data %>% mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P10/GM)/qnorm(.10)))) %>%
+    mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P25/GM)/qnorm(.25)))) %>%
+    mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P75/GM)/qnorm(.75)))) %>%
+    mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P90/GM)/qnorm(.90)))) %>%
+    mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P95/GM)/qnorm(.95)))) %>%
+    mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P99/GM)/qnorm(.99))))
+
+  # D. Estimate GSD using Pleil 2.
+  data <- suppressWarnings(data %>% mutate(GSD = if_else(!is.na(GSD),GSD ,exp(sqrt(2 * log(Mean/GM))))))
+  # E. Estimate GM using Pleil 3.
+  data <- data %>% mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(Max/GM)/qnorm(1-1/`Sample Size`))))
+  data <- data %>% mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(Min/GM)/qnorm(1/`Sample Size`))))
+
+  # F. Estimate Mean and SD using "Estimating.datalsdata" Methods (5) and (16). Mean calculated from min, median, Maximum,
+  # and SD from minimum, median, Maximum, and range.
+
+  data <- data %>% mutate(Mean = if_else(!is.na(Mean),Mean, (Min+2*Median+Max)/4))
+  data <- data %>% mutate(SD = if_else(!is.na(SD),SD, sqrt ((1/12) * ((Min-2*Median+Max)^2)/4 + (Max-Min)^2)))
+
+
+  # G. Estimate SD using Ramirez & Codata Method and range rule. Applied only if sample size  > 10.
+  data <- data %>% mutate(SD = if_else((!is.na(SD) & `Sample Size` > 10),SD, (Max-Min)/ (3*sqrt(log(`Sample Size`))-1.5)))
+  data <- data %>% mutate(SD = if_else((!is.na(SD) & `Sample Size` > 10),SD, (Max-Min)/4))
+
+  # ______________________________ Repeat A - E. ______________________________ #
+
+
+  # H. Estimate GM using Pleil 1.
+  data <- data %>% mutate(GM = if_else(!is.na(GM),GM , Median))
+
+  # I. Estimate GM using Pleil 2.
+  data <- data %>% mutate(GM = if_else(!is.na(GM),GM , Mean/(1+0.5 *(SD/Mean)^2)))
+
+  # J. Estimate GSD using Pleil 1.
+  data <- data %>% mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P10/GM)/qnorm(.10)))) %>%
+    mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P25/GM)/qnorm(.25)))) %>%
+    mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P75/GM)/qnorm(.75)))) %>%
+    mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P90/GM)/qnorm(.90)))) %>%
+    mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P95/GM)/qnorm(.95)))) %>%
+    mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(P99/GM)/qnorm(.99))))
+
+  # K. Estimate GSD using Pleil 2.
+  data <- suppressWarnings(data %>% mutate(GSD = if_else(!is.na(GSD),GSD ,exp(sqrt(2 * log(Mean/GM))))))
+
+  # L. Estimate GM using Pleil 3.
+  data <- data %>% mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(Max/GM)/qnorm(1-1/`Sample Size`))))
+  data <- data %>% mutate(GSD = if_else(!is.na(GSD),GSD ,exp(log(Min/GM)/qnorm(1/`Sample Size`))))
+
+
+  df <- data.frame(A=1:10, B=2:11, C=3:12)
+  fun1 <- function(x, column){
+    max(x[,column])
+  }
+
+  set.seed(seed)
+  wgm  <- weighted.mean(data$GM, data[,wt])
+  wgsd <- weighted.mean(data$GSD, data[,wt])
+
+  Concentration<- rlnorm(n,wgm,wgsd)
+  Exposure     <- rlnorm(n,wgm,wgsd)*expf
+
+  sumstats<- function(x){
+    sumstats <- t(quantile(x,c(0,.10,.5,.75,.95,1)))
+    mymean   <- mean(x)
+    sumstats <- cbind(mymean,sumstats)
+    colnames(sumstats)<-c("Mean","Min","10th%","Median","75th%","95th","Max")
+    sumstats<- signif(sumstats,5)
+    return(sumstats)
+  }
+
+  summary <- rbind(sumstats(Concentration),sumstats(Exposure))
+  summary <- cbind(c("Concentration", "Exposure"),summary)
+  colnames(summary)[1] <- "Result"
+
+
+  Concentration <- data.frame(Concentration)
+  Concentration$Units <- names(table(data$Units))
+
+  Exposure <- data.frame(Exposure)
+  Exposure$Units <- expunits
+
+  namer <- function(name){
+
+    date   <- Sys.time()
+    year   <- substr(date,1,4)
+    month  <- substr(date,6,7)
+    day    <- substr(date,9,10)
+    hour   <- as.numeric(substr(date,12,13))
+    minute <- substr(date,15,16)
+
+    if (hour > 12){
+      hour <- as.character(hour-12)
+      minute <- paste0(minute,"PM")
+    } else {
+      minute <- paste0(minute,"AM")
+    }
+
+    filename <- paste0(name," ",hour,minute," ",month," ",day," ",year)
+
+    return(filename)
+  }
+  time  <- namer('Run Date:')
+  description <- t(data.frame(Info =time,
+                              n,
+                              WGM = wgm,
+                              WGSD= wgsd,
+                              Weight = wt,
+                              Seed = seed))
+
+
+
+
+  # return( sumstats, dist, data, cover)
+  results<-list(description,summary,Exposure,Concentration)
+  names(results)<- c("Description","Summary Statistics","Exposure","Concentration")
+
+  return(results)
+}
+LEM(data,"Sample Size",1,"ng/g",25,1)
 
