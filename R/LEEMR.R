219763b2a4b8bbde1438a806a40407d21be81395
East <East.Alexander@epa.gov>
2022-01-19 11:29:59 -0500

    Trying roxygen

---
+++ R/LEEMR.R
@@ -0,0 +1,12 @@
+#' LEEMR: Lognormal Concentration & Exposure Distributions from sparse summary statistics
+#'
+#' The Lorber Egeghy East Model R package (LEEMR):
+#' Has 3 functions
+#'
+#' @section functions:
+#' xyz
+#'
+#' @docType package
+#' @name LEEMR
+NULL
+#> NULL
